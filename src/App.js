e3e78543b650b45d37d8967a4c9a07304a7e913d
Ashutosh Mittal <ashutoshsanjeevmittal@gmail.com>
2023-11-11 19:35:28 +0530

    remove

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/navbar/Navbar';
-import Home from './components/home/Home';
-import Start from '/home/ubuntu/project/arweave/src/components/start/Start.js';
-import About from '/home/ubuntu/project/arweave/src/components/about/About.js';
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import React from "react";
+import "./App.css";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/home/Home";
+import Start from "/home/ubuntu/project/arweave/src/components/start/Start.js";
+import About from "/home/ubuntu/project/arweave/src/components/about/About.js";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
@@ -14,17 +14,16 @@ function App() {
     // </div>
     // </div>
     <BrowserRouter>
-    <Navbar/>
-    <Routes>
-      <Route path='/' element={<Home/>} />
-      <Route path='/start' element={<Start/>} />
-      <Route path='/about' element={<About/>} />
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/start" element={<Start />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </BrowserRouter>
   );
 }
 
 export default App;
-
 
 //npm install react-icons --save
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,43 +1,44 @@
 import React from "react";
 import "./Navbar.css";
 import { NavLink } from "react-router-dom";
-import { ArweaveWebWallet } from 'arweave-wallet-connector';
-
+import { ArweaveWebWallet } from "arweave-wallet-connector";
 
 function Navbar() {
+  const connect_wallet = async () => {
+    const wallet = new ArweaveWebWallet({
+      name: "AAshu",
+      logo: "https://i.pinimg.com/564x/ed/64/96/ed64960142ea3db49cefad283f9f33c2.jpg",
+    });
 
-  const connect_wallet =async()=>{
-    const wallet = new ArweaveWebWallet({ 
-      name: 'AAshu',
-      logo: 'https://i.pinimg.com/564x/ed/64/96/ed64960142ea3db49cefad283f9f33c2.jpg'
-    })
-    
-    wallet.setUrl('https://arweave.app')
-   const no= await wallet.connect() 
-  console.log("dawdawdawdawd :::: "+no);
-  
-  }
+    wallet.setUrl("https://arweave.app");
+    const no = await wallet.connect();
+    console.log("dawdawdawdawd :::: " + no);
+  };
 
-  
   return (
-   
     <nav className="main-nav">
       <div className="logo">
-       <NavLink to="/"><h2>Ar-KYC</h2></NavLink> 
+        <NavLink to="/">
+          <h2>Ar-KYC</h2>
+        </NavLink>
       </div>
       <div className="header-content">
         <ul>
-          <li><NavLink to="/">Home</NavLink></li>
-          <li><NavLink to="/start">Start</NavLink></li>
-          <li><NavLink to="/about">About</NavLink></li>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/start">Start</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
         </ul>
       </div>
       <div className="connect-button">
         <button onClick={connect_wallet}>Connect Wallet</button>
-     
       </div>
     </nav>
-  
   );
 }
 
