e1fedeb46f1dcc7cd6e98d5a1c09f213088a7a6b
Ashutosh Mittal <ashutoshsanjeevmittal@gmail.com>
2023-12-31 05:22:39 +0530

    modify contract and some other changes

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import "./About.css";
 
 function About(){
     return (
         <div>
-          <h1 style={{color:"red"}}>Aboutttttttt</h1>
+          {/* <h1 style={{color:"red"}}>Aboutttttttt</h1> */}
+          <ul className='abt'>
+            <li><h1>What is AR-KYC?</h1>
+            <h2>AR-KYC is an innovative solution leveraging blockchain technology to simplify and enhance the complexity inherent in the KYC process.</h2>
+            </li>
+            <li><h1>What does it solve?</h1>
+            <h2>AR-KYC is an innovative solution leveraging blockchain technology to simplify and enhance the complexity inherent in the KYC process.</h2>
+            </li>
+          </ul>
         </div>
         
       );
--- src/components/contract/kyc.js
+++ src/components/contract/kyc.js
@@ -1,47 +1,112 @@
-export function handle(state, action) {
-  const company = state.company; //state variable
-  const people = state.people; //state variable
-  const owners = state.owners; //state variable
+export async function handle(state, action) {
+  const company = state.company; // state variable
+  const people = state.people; // state variable
+  const owners = state.owners; // state variable
+  const map1 = state.map1; // state variable
+  const num1 = state.num1; // state variable
+
   const input = action.input;
   const caller = action.caller;
 
-  //    if (input.function === "initialize") {
-  //           owners=caller;
-  //   }
+  switch (action.input.function) {
+    case "upload_kyc": {
+      const comp = input.comp; // input - variable
+      const kyc = input.kyc; // input - object
 
-  if (input.function === "companyReg" && caller === SmartWeave.contract.owner) {
-    var _companyReg_addrs = input.companyReg_addrs;
-    var _companyReg_access = true;
-    var _companyReg_name = input.companyReg_name;
-    var _companyReg_regID = input.companyReg_regID;
-    var Id = 0;
-    Id = Id + 1;
-    company[input.company.id] = {
-      id: Id,
-      issuerAddress: _companyReg_addrs,
-      access: _companyReg_access,
-      organisationName: _companyReg_name,
-      companyRegisteredNumber: _companyReg_regID,
-    };
-    return { state };
-  }
+      if (kyc.name == undefined) {
+        throw new ContractError("Please enter the name");
+      }
+      if (kyc.age == undefined) {
+        throw new ContractError("Please enter the age");
+      }
+      if (kyc.nation == undefined) {
+        throw new ContractError("Please enter the nationality");
+      }
+      if (kyc.ph_no == undefined) {
+        throw new ContractError("Please enter the phone number");
+      }
+      if (kyc.dl == undefined) {
+        throw new ContractError("Please enter the driving license number");
+      }
 
-  if (input.function === "uploadKyc") {
-    var _uploadKyc_name = input.uploadKyc_name;
-    var _uploadKyc_age = input._uploadKyc_age;
-    var _uploadKyc_nationality = input._uploadKyc_nationality;
-    var _uploadKyc_IP = input._uploadKyc_IP;
-    var _uploadKyc_DL = input._uploadKyc_DL;
-    var Id = 0;
-    Id = Id + 1;
-    people[input.people.id] = {
-      id: Id,
-      name: _uploadKyc_name,
-      age: _uploadKyc_age,
-      nationality: _uploadKyc_nationality,
-      identity_proof: _uploadKyc_IP,
-      driving_license: _uploadKyc_DL,
-    };
-    return { state };
+      if (!Number.isInteger(_uploadKyc_ph && _uploadKyc_age)) {
+        throw new ContractError(
+          'Invalid value for "age and phone number". Must be an integer.'
+        );
+      }
+
+      kyc.access = true;
+      kyc.address = caller;
+
+      if (state.people[comp] == undefined) {
+        state.people[comp] = 0;
+      }
+
+      const key = `${comp} ${caller}`;
+
+      var comp_id = state.people[comp];
+      state.people[comp][comp_id] = kyc;
+      state.map1[key] = state.people[comp][comp_id];
+      state.people[comp]++;
+
+      return { state };
+    }
+
+    case "company_reg": {
+      const comp = input.comp; // input - object
+
+      if (comp.name == undefined) {
+        throw new ContractError("Please enter the company name");
+      }
+      if (comp.category == undefined) {
+        throw new ContractError("Please enter the type of company");
+      }
+      comp.address = caller;
+
+      state.company[num2] = comp;
+      num2++;
+
+      return { state };
+    }
+
+    case "getDetails": {
+      const comp = input.comp; // input - variable
+      const address = input.address; // input - variable
+
+      const key = `${comp} ${address}`;
+
+      if (!(key in state.map1)) {
+        throw new ContractError(
+          "This address doesn't give you their KYC details"
+        );
+      }
+
+      const x = state.map1[key];
+      if (x.access == false) {
+        throw new ContractError(
+          "This address doesn't give your company to access their KYC details"
+        );
+      }
+
+      return { result: state.map1[key] };
+    }
+
+    case "rmAccessU": {
+      const comp = input.comp; // input - variable
+
+      const key = `${comp} ${caller}`;
+
+      if (!(key in state.map1)) {
+        throw new ContractError("Wrong details");
+      }
+
+      state.map1[key].access = false;
+
+      return { state };
+    }
+
+    default: {
+      throw new ContractError("Unsupported contract function");
+    }
   }
 }
