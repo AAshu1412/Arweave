27504952938a693ff6ffd2626c0cde148ccd6a89
Ashutosh Mittal <ashutoshsanjeevmittal@gmail.com>
2024-01-01 23:57:31 +0530

    new test 2

--- src/components/contract/test.js
+++ src/components/contract/test.js
@@ -4,19 +4,34 @@ const Arweave = require('arweave');
 const { SmartWeaveNodeFactory, LoggerFactory } = require("redstone-smartweave");
 const { default: ArLocal } = require("arlocal");
 
-(async () => {
+test('test transaction', async () => {
   
 // Set up ArLocal
-const arLocal = new ArLocal(1984, false);
+const arLocal = new ArLocal();
 await arLocal.start();
 
 // Set up Arweave client
-const arweave = Arweave.init({
-  host: "localhost",
-  port: 1984,
-  protocol: "http"
-});
-const wallet = await arweave.wallets.generate();
+const arweave = Arweave.init({});
+// arweave.wallets.generate().then((key) => {
+//   console.log(key);
+//   const wallet = key;
+
+//   // {
+//   //     "kty": "RSA",
+//   //     "n": "3WquzP5IVTIsv3XYJjfw5L-t4X34WoWHwOuxb9V8w...",
+//   //     "e": ...
+// });
+
+// arweave.wallets.jwkToAddress(key).then((address) => {
+//   console.log(address);
+//   const walletAddress=address;
+//   //1seRanklLU_1VTGkEk7P0xAwMJfA7owA1JHW5KyZKlY
+// });
+
+ // airdrop amount of tokens (in winston) to wallet
+ await arweave.api.get(`mint/yWEDs-sho-5Ka7ql_Ov71GNFdHqLspekxfhAo1bcqtU/10000000000000000`)
+
+  // create mine function
 const mine = () => arweave.api.get("mine");
 
 // Set up SmartWeave client
