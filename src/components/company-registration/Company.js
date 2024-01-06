8d7875a653c280f01d2335736e816926d0ed2102
Ashutosh Mittal <ashutoshsanjeevmittal@gmail.com>
2024-01-06 08:23:29 +0530

    finally found the img for comp page

--- src/components/company-registration/Company.js
+++ src/components/company-registration/Company.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 import { useState } from "react";
-import Start from '../start/Start';
-import './Company.css';
+import Start from "../start/Start";
+import "./Company.css";
 
-function Company(){
-    const [compDetails, setcompDetails] = useState({ name: "", category: "" });
-  
+function Company() {
+  const [compDetails, setcompDetails] = useState({ name: "", category: "" });
+
   const handleInput = (event) => {
     const name = event.target.name;
     const value = event.target.value;
@@ -17,56 +17,55 @@ function Company(){
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    
+    console.log("Submit button is working!");
   };
 
-    return(<div>
-        <Start/>
-        <div className="company-main">
+  return (
+    <div>
+      <Start />
+      <div className="company-main">
         <div className="company-img">
           <img
-            src="https://i.pinimg.com/564x/01/b0/38/01b0380831048a6dbef6a29fca4c3ee9.jpg"
+            src="https://i.pinimg.com/564x/3f/54/98/3f5498a8b6b8111ba41d2ccf388c7970.jpg"
             width="508"
-            height="906"
+            height="685.75"
             alt="me"
           ></img>
         </div>
-       
-          <div className="company-form">
-            <form onSubmit={handleSubmit}>
-              <div>
-                <label htmlFor="name">Enter Your Company Name : </label>
-                <input
-                  type="text"
-                  autoComplete="off"
-                  value={compDetails.name}
-                  onChange={handleInput}
-                  name="name"
-                  id="name"
-                  placeholder="Company Name"
-                ></input>
-              </div>
 
-              <div>
-                <label htmlFor="category">Enter Your Company Category : </label>
-                <input
-                  type="text"
-                  autoComplete="off"
-                  value={compDetails.category}
-                  onChange={handleInput}
-                  name="category"
-                  id="category"
-                  placeholder="Company Category"
-                ></input>
-              </div>
-              <button type="submit">Submit</button>
-            </form>
-          </div>
-         
+        <div className="company-form">
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="name">Enter Your Company Name : </label>
+              <input
+                type="text"
+                autoComplete="off"
+                value={compDetails.name}
+                onChange={handleInput}
+                name="name"
+                id="name"
+                placeholder="Company Name"
+              ></input>
+            </div>
+
+            <div>
+              <label htmlFor="category">Enter Your Company Category : </label>
+              <input
+                type="text"
+                autoComplete="off"
+                value={compDetails.category}
+                onChange={handleInput}
+                name="category"
+                id="category"
+                placeholder="Company Category"
+              ></input>
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
       </div>
-
-    </div>);
-
+    </div>
+  );
 }
 
 export default Company;
