822db3291dc51e39aa140113c41ef74ee5fe3cdd
Ashutosh Mittal <ashutoshsanjeevmittal@gmail.com>
2024-01-08 02:19:14 +0530

    modify reg page 1

--- src/components/registration/Registration.js
+++ src/components/registration/Registration.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import Start from '../start/Start';
+import './Registration.css';
 
 function Registration(){
     return(<div>
         <Start/>
-             <h1 style={{color:"red"}}>registrationnnnnnnnnn</h1>
+            <div className='registration-main'>
+                <div className='registration-comp'>
+                     <h2>HDFC</h2>
+                     <div className='registration-comp-sub'>
+                     <h4>Bank</h4>
+                     <h4>yWEDs-sho-5Ka7ql_Ov71GNFdHqLspekxfhAo1bcqtU</h4>
+                     </div>
+                    
+                </div>
+                <div className='registration-user'>
+                   
+                </div>
+            </div>
     </div>);
 
 }
