6c6893004ace20ce5c6d450cfc206baeb2862725
Ashutosh Mittal <ashutoshsanjeevmittal@gmail.com>
2024-01-08 05:21:31 +0530

    modify reg page 3

--- src/components/registration/Registration.js
+++ src/components/registration/Registration.js
@@ -37,6 +37,55 @@ const handleSubmit=()=>{
                 placeholder="Name"
               ></input>
             </div>  
+            <div>
+              <label htmlFor="age">Enter Your Age : </label>
+              <input
+                type="text"
+                autoComplete="off"
+                value="age"
+                onChange={handleInput}
+                name="age"
+                id="age"
+                placeholder="Age"
+              ></input>
+            </div>  
+            <div>
+              <label htmlFor="nation">Enter Your Nationality : </label>
+              <input
+                type="text"
+                autoComplete="off"
+                value="nation"
+                onChange={handleInput}
+                name="nation"
+                id="nation"
+                placeholder="Nationality"
+              ></input>
+            </div>  
+            <div>
+              <label htmlFor="ph_no">Enter Your Phone Number : </label>
+              <input
+                type="text"
+                autoComplete="off"
+                value="ph_no"
+                onChange={handleInput}
+                name="ph_no"
+                id="ph_no"
+                placeholder="Phone Number"
+              ></input>
+            </div>  
+            <div>
+              <label htmlFor="dl">Enter Your Driving License ID : </label>
+              <input
+                type="text"
+                autoComplete="off"
+                value="dl"
+                onChange={handleInput}
+                name="dl"
+                id="dl"
+                placeholder="Driving License"
+              ></input>
+            </div>  
+            
 
             <button type="submit">Submit</button>
           </form>
